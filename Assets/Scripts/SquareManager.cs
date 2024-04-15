4f06af80efddf498a0a2df13d6c8f961e49e5885
Caleb Harris <caleb@Calebs-Air.lan>
2024-04-15 18:51:25 -0400

    fix/bombs fixed

--- Assets/Scripts/SquareManager.cs
+++ Assets/Scripts/SquareManager.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:61ae7a5f9803a0846d0e3ef0693559d0245e1e17e928b4aca2920f98a4a4c287
-size 8322
+oid sha256:1504e703164f4b0983c724b1d0800b7cda76d3bc6acc60407dcf0461ca974b07
+size 8193
