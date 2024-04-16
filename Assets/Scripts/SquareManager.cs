fde0c21d74fc9aae950973d8dc8ce9541abe5b5c
Caleb Harris <caleb@Calebs-Air.lan>
2024-04-15 20:02:45 -0400

    fix/bombs inside square logic fixed

--- Assets/Scripts/SquareManager.cs
+++ Assets/Scripts/SquareManager.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1504e703164f4b0983c724b1d0800b7cda76d3bc6acc60407dcf0461ca974b07
-size 8193
+oid sha256:dfb5eb3c47a3f71cea58a54579cefa01f5c292e9a9ce3988ae1d3dc5b338cf87
+size 8419
