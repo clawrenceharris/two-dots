5a34b53f1c3ddfd2a40b3f30a09b3364ab84e658
Caleb <clawrenceharris@gmail.com>
2024-07-04 16:48:34 -0400

    file restructuring

---
+++ Assets/Scripts/Tiles/Tile/Tile.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using DG.Tweening;
+using UnityEngine;
+using static Type;
+public abstract class Tile : DotsGameObject
+{
+    
+    public abstract TileType TileType { get;}
+    public VisualController VisualController => GetVisualController<VisualController>();
+}
