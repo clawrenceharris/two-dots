4ee4d9a851c54954ff4b9af8efceaf9fcc3534c7
Caleb <clawrenceharris@gmail.com>
2024-07-16 18:49:49 -0400

    input improved and fixes

--- Assets/Scripts/Input/DotTouchIO.cs
+++ Assets/Scripts/Input/DotTouchIO.cs
@@ -2,28 +2,35 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using System;
+using System.Linq;
 public class DotTouchIO : MonoBehaviour
 {
-    private ConnectableDot connectable;
+    private ConnectableDot dot;
     
 
     public static event Action<ConnectableDot> onDotSelected;
     public static event Action<ConnectableDot> onDotConnected;
     public static event Action onSelectionEnded;
-    private float offset = 0.5f;
+    private float Offset
+    {
+        get
+        {
+            return 0.5f;
+        }
+    }
     private static bool selectionEnded;
     public static bool IsInputActive { get; set; }
-
-    private void Awake() => connectable = GetComponent<ConnectableDot>();
+    private readonly Stack<ConnectableDot> selectedDots = new();
+    private void Awake() => dot = GetComponent<ConnectableDot>();
 
     private bool IsDotAt(Vector3 worldPosition)
     {
-        if(connectable is not IBoardElement boardElement)
+        if(dot is not IBoardElement boardElement)
         {
             return false;
         }
 
-        if(connectable.HitCount >= connectable.HitsToClear)
+        if(dot.HitCount >= dot.HitsToClear)
         {
             return false;
         }
@@ -34,22 +41,33 @@ public class DotTouchIO : MonoBehaviour
         return column == boardElement.Column && row == boardElement.Row;
         
     }
-   
+    private IEnumerator AddAndRemoveSelectedDot(ConnectableDot dot)
+    {
+        selectedDots.Push(dot);
+        yield return new WaitForSeconds(DotVisuals.selectionAnimationDuration);
+        selectedDots.Pop();
+    }
     private void CheckInput()
     {
 
-        Vector3 worldPos = (Camera.main.ScreenToWorldPoint(Input.mousePosition) / Board.offset) + Vector3.one * offset;
+        Vector3 worldPos = (Camera.main.ScreenToWorldPoint(Input.mousePosition) / Board.offset) + Vector3.one * Offset;
         if (Input.GetMouseButtonDown(0) && IsDotAt(worldPos))
         {
+            if (selectedDots.Contains(dot))
+            {
+                return;
+            }
+
+            StartCoroutine(AddAndRemoveSelectedDot(dot));
             IsInputActive = true;
             selectionEnded = false;
-            onDotSelected?.Invoke(connectable);
+            onDotSelected?.Invoke(dot);
         }
 
         else if (Input.GetMouseButton(0) && IsDotAt(worldPos))
         {
            
-            onDotConnected?.Invoke(connectable);
+            onDotConnected?.Invoke(dot);
         }
 
         else if (Input.GetMouseButtonUp(0) && !selectionEnded)
@@ -65,7 +83,7 @@ public class DotTouchIO : MonoBehaviour
     void Update()
     {
         
-        if(CommandInvoker.CommandsEnded)
+        if(!CommandInvoker.Instance.IsExecuting)
             CheckInput();
 
     }
