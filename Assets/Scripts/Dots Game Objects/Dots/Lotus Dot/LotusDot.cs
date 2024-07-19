a4d4d4e0eb652f2f9081a85320f8b7cba4c47731
Caleb <clawrenceharris@gmail.com>
2024-07-19 14:19:12 -0400

    lotus dot created and implemented

---
+++ Assets/Scripts/Dots Game Objects/Dots/Lotus Dot/LotusDot.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using static Type;
+public class LotusDot : ConnectableDot
+{
+    public override DotType DotType => DotType.LotusDot;
+    public override int HitsToClear => 1;
+
+    public override Dictionary<HitType, IHitRule> HitRules => new(){{HitType.LotusDot, new HitByAdjacentColorRule()}};
+
+    public override void Deselect()
+    {
+       //do nothing
+    }
+    public override void Connect(ConnectableDot dot)
+    {
+        //do nothing
+    }
+    public override void Hit(HitType hitType)
+    {
+        HitCount++;
+    }
+
+    public override void InitDisplayController()
+    {
+        visualController = new LotusDotVisualController();
+        visualController.Init(this);
+    }
+
+    
+}
