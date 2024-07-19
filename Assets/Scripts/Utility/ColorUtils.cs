8085f0f488cdc381591ee2b1333ef1df144c6558
Caleb <clawrenceharris@gmail.com>
2024-07-19 14:12:18 -0400

    utility folder added and color utility class

---
+++ Assets/Scripts/Utility/ColorUtils.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColorUtils
+{
+    /// <summary>
+    /// Lightens a given color by a specified amount.
+    /// </summary>
+    /// <param name="color">The original color.</param>
+    /// <param name="amount">The amount to lighten the color, where 0 means no change and 1 means fully white.</param>
+    /// <returns>The lightened color.</returns>
+    public static Color LightenColor(Color color, float amount)
+    {
+        // Clamp the amount to ensure it is between 0 and 1
+        amount = Mathf.Clamp01(amount);
+
+        // Interpolate between the original color and white
+        return Color.Lerp(color, Color.white, amount);
+    }
+
+    /// <summary>
+    /// Darkens a given color by a specified amount.
+    /// </summary>
+    /// <param name="color">The original color.</param>
+    /// <param name="amount">The amount to lighten the color, where 0 means no change and 1 means fully white.</param>
+    /// <returns>The lightened color.</returns>
+    public static Color DarkenColor(Color color, float amount)
+    {
+        // Clamp the amount to ensure it is between 0 and 1
+        amount = Mathf.Clamp01(amount);
+
+        // Interpolate between the original color and white
+        return Color.Lerp(color, Color.black, amount);
+    }
+}
