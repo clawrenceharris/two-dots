dec577e28c5137571b373ef057fa3ac90199b815
Caleb <clawrenceharris@gmail.com>
2024-07-19 22:00:14 -0400

    water hittable logic implmented

--- Assets/Scripts/Dots Game Objects/Tiles/Water/Water.cs
+++ Assets/Scripts/Dots Game Objects/Tiles/Water/Water.cs
@@ -1,18 +1,37 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
-public class Water : MonoBehaviour
+public class Water : Tile, IHittable
 {
-    // Start is called before the first frame update
-    void Start()
+    public override TileType TileType => TileType.Water;
+
+    private readonly HittableBase hittable = new();
+
+
+    public HitType HitType { get => hittable.HitType; protected set => hittable.HitType = value; }
+
+    public int HitCount { get => hittable.HitCount; set => hittable.HitCount = value; }
+
+    public bool WasHit { get => hittable.WasHit; set => hittable.WasHit = value; }
+
+    public int HitsToClear => int.MaxValue;
+
+    public Dictionary<HitType, IHitRule> HitRules => new(){{HitType.Water, new HitBySamePositionRule()}};
+
+    public IEnumerator Clear()
     {
-        
+        yield return hittable.Clear();
     }
 
-    // Update is called once per frame
-    void Update()
+    public IEnumerator Hit(HitType hitType, Action onHitComplete = null)
     {
-        
+        yield return Hit(hitType, onHitComplete);
+    }
+
+    public override void InitDisplayController()
+    {
+        visualController = new WaterVisualController();
+        visualController.Init(this);
     }
 }
