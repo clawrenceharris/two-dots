6eac97a2ebe3ecafa391e8c93e38fdfcd656197a
Caleb <clawrenceharris@gmail.com>
2024-08-07 18:17:21 -0400

    numerable visuals refactored

--- Assets/Scripts/Visuals/Numerable Visuals/NumerableVisualController.cs
+++ Assets/Scripts/Visuals/Numerable Visuals/NumerableVisualController.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections;
+using System.Collections.Generic;
+using DG.Tweening;
 using Unity.VisualScripting;
 using UnityEngine;
 
@@ -7,18 +9,46 @@ public class NumerableVisualController : INumerableVisualController
 
 {
     private INumerableVisuals visuals;
+    private INumerable numerable;
+
     
-
-    public IEnumerator DoIdleAnimation()
-    {
-        throw new NotImplementedException();
-    }
-
     public void Init(INumerable numerable, INumerableVisuals visuals)
     {
         this.visuals = visuals;
+        this.numerable = numerable;
     }
     
+    public void UpdateNumberByConnectionCount(ConnectableDot dot){
+        
+        if(!ConnectionManager.ConnectedDots.Contains(dot)){
+            return;
+        }
+       
+        if(ConnectionManager.ConnectedDots.Count == 1){
+            numerable.TempNumber = numerable.CurrentNumber;
+            UpdateNumbers(numerable.TempNumber);
+            return;
+        }
+        List<IHittable> toHit = ConnectionManager.ToHit;
+        numerable.TempNumber = Mathf.Clamp(numerable.CurrentNumber - toHit.Count, 0, int.MaxValue);
+        
+        UpdateNumbers(numerable.TempNumber);
+        
+
+        CoroutineHandler.StartStaticCoroutine(ScaleNumbers());
+    }
+
+    public IEnumerator ScaleNumbers()
+    {
+        float scaleDuration = 0.2f;
+
+        visuals.Digit1.transform.DOScale(Vector2.one * 1.8f, scaleDuration);
+        visuals.Digit2.transform.DOScale(Vector2.one * 1.8f, scaleDuration);
+
+        yield return new WaitForSeconds(scaleDuration);
+        visuals.Digit1.transform.DOScale(Vector2.one, scaleDuration);
+        visuals.Digit2.transform.DOScale(Vector2.one, scaleDuration);
+    }
 
     public void UpdateNumbers(int number)
     {
