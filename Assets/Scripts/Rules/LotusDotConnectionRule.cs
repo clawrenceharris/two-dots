47b1ba99e8580ded6f3f94e3ad915acc8c62f650
Caleb <clawrenceharris@gmail.com>
2024-08-15 16:22:49 -0400

    lotus dot connection rule takes IConnectable objects

--- Assets/Scripts/Rules/LotusDotConnectionRule.cs
+++ Assets/Scripts/Rules/LotusDotConnectionRule.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using System.Linq;
-public class LotusDotConnectionRule : ConnectionRule
+public class LotusDotConnectionRule : IComparisonRule<IConnectable>
 {
     /// <summary>
     /// Checks if a given lotus dot can be connected to another given dot.
@@ -12,13 +12,13 @@ public class LotusDotConnectionRule : ConnectionRule
     /// <param name="b">The other dot to compare</param>
     /// <param name="board">The game board</param>
     /// <returns>Whether the lotus dot can be connected to</returns>
-    public override bool Validate(ConnectableDot a, ConnectableDot b, Board board)
+    public bool Validate(IConnectable a, IConnectable b, Board board)
     {
         if(a.Color == b.Color)
         {
             return true;
         }
-        if (b.DotType.IsMoonstoneDot())
+        if (b is Dot dot && dot.DotType.IsMoonstoneDot())
         {
             return true;
         }
