cd0753114e42651b661e0c7c5eed6c3103fcddf6
Caleb <clawrenceharris@gmail.com>
2024-08-15 16:21:08 -0400

    gem now extends ConnectableDot

--- Assets/Scripts/Dots Game Objects/Dots/Gem/Gem.cs
+++ Assets/Scripts/Dots Game Objects/Dots/Gem/Gem.cs
@@ -3,13 +3,13 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Gem : Dot, IExplodable, IColorable, IPreviewable
+public class Gem : ConnectableDot, IExplodable, IPreviewable
 {
 
     public override DotType DotType => DotType.Gem;
     private static readonly List<IHittable> visited = new();
 
-    public override IHitRule HitRule => new HitBySquareRule();
+    public override IHitRule HitRule => new HitByConnectionRule();
 
     public override int HitsToClear => 1;
 
@@ -19,7 +19,6 @@ public class Gem : Dot, IExplodable, IColorable, IPreviewable
 
     public int HitsToExplode => 1;
 
-    public DotColor Color { get; set; }
 
     public new GemVisualController VisualController => GetVisualController<GemVisualController>();
    
@@ -51,10 +50,7 @@ public class Gem : Dot, IExplodable, IColorable, IPreviewable
         visualController.Init(this);
     }
 
-    public void Select()
-    {
-        StartCoroutine(VisualController.AnimateSelectionEffect());
-    }
+    
 
     public bool ShouldPreviewClear(Board board)
     {
@@ -69,6 +65,11 @@ public class Gem : Dot, IExplodable, IColorable, IPreviewable
     public override void Hit(HitType hitType)
     {
         
+    }
+
+    public override void Deselect()
+    {
+        //do nothing
     }
 }
 
