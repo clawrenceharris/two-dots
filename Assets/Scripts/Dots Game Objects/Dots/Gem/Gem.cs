f18a7c64cc14e53fffecd153c6625d47d8f4da0c
Caleb <clawrenceharris@gmail.com>
2024-08-19 17:33:22 -0400

    rectangle and square gems implemented

--- Assets/Scripts/Dots Game Objects/Dots/Gem/Gem.cs
+++ Assets/Scripts/Dots Game Objects/Dots/Gem/Gem.cs
@@ -3,49 +3,32 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using System.Linq;
+using DG.Tweening.Core;
 public abstract class Gem : ConnectableDot, IExplodable, IPreviewable
 {
 
-    public override DotType DotType => DotType.Gem;
     private static readonly List<IHittable> visited = new();
 
     public override IHitRule HitRule => new HitByConnectionRule();
 
     public override int HitsToClear => 1;
 
-    public IExplosionRule ExplosionRule => new GemExplosionRule();
+    public abstract IExplosionRule ExplosionRule {get;}
 
     public ExplosionType ExplosionType => ExplosionType.GemExplosion;
 
     public int HitsToExplode => 1;
-
-
-    public new GemVisualController VisualController => GetVisualController<GemVisualController>();
    
+    public new GemVisualController VisualController => GetVisualController<GemVisualController>();
 
     public IEnumerator Explode(List<IHittable> toHit, Board board, Action<IHittable> onComplete= null)
     { 
         HitCount++;
-       foreach(IHittable hittable in toHit){
-            
-            if(!visited.Contains(hittable) && hittable is Gem g){
-                visited.Add(g);
-                onComplete?.Invoke(g);                
-            }
-            
-        }
-
-        yield return new WaitForSeconds(0.5f);
-
-    }
-
     
-   
-    public override void InitDisplayController()
-    {
-        visualController = new GemVisualController();
-        visualController.Init(this);
+        yield return VisualController.Explode();
+
     }
+
 
     
 
--- Assets/Scripts/Dots Game Objects/Dots/Gem/SquareGem.cs
+++ Assets/Scripts/Dots Game Objects/Dots/Gem/SquareGem.cs
@@ -4,7 +4,20 @@ using UnityEngine;
 
 public class SquareGem : Gem
 {
+    public override DotType DotType => DotType.SquareGem;
+
+    public new SquareGemVisualController VisualController => GetVisualController<SquareGemVisualController>();
+
+    public override IExplosionRule ExplosionRule => new SquareGemExplosionRule();
+
     private void OnDestroy(){
         VisualController.Unsubscribe();
     }
+
+     public override void InitDisplayController()
+    {
+        visualController = new SquareGemVisualController();
+        visualController.Init(this);
+    }
+
 }
