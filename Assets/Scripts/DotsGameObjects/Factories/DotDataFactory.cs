d390af2ee9babac05a1589682651b4a76c84be43
Caleb <clawrenceharris@gmail.com>
2024-09-07 17:06:33 -0400

    factories folder created for all factory scrpts

---
+++ Assets/Scripts/DotsGameObjects/Factories/DotDataFactory.cs
@@ -0,0 +1,69 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using Newtonsoft.Json.Linq;
+
+
+public class DotDataFactory
+{
+    public static DotsGameObjectData CreateDotData(JObject itemObject)
+    {
+        string type = (string)itemObject["type"];
+        JToken col = itemObject["col"];
+        JToken row = itemObject["row"];
+        JToken color = itemObject["color"];
+        JToken colors = itemObject["colors"];
+        JToken hitCount = itemObject["hitCount"];
+        
+        JToken directionX = itemObject["directionX"];
+        JToken directionY = itemObject["directionY"];
+        JToken number = itemObject["number"];
+        DotsGameObjectData dotData = new(type)
+        {
+            hitCount = hitCount != null ? (int)hitCount : 0,
+            col = col != null ? (int)col : -1,
+            row = row != null ? (int)row : -1,
+
+        };
+       
+        switch (type)
+        {
+            case "lotus":
+            case "normal":
+                dotData.SetProperty("Color", (string)color);
+                break;
+            case "beetle":
+                dotData.SetProperty("Colors", colors.ToObject<string[]>());
+                dotData.SetProperty("Color", (string)color);
+
+                dotData.SetProperty("DirectionX", (int)directionX);
+                dotData.SetProperty("DirectionY", (int)directionY);
+                break;
+            case "clock":
+                dotData.SetProperty("Number", (int)number);
+                dotData.SetProperty("Color", JSONLevelLoader.ToJsonColor(DotColor.Blank));
+
+                break;
+            case "monster":
+                dotData.SetProperty("Color", (string)color);
+                dotData.SetProperty("Number", (int)number);
+
+                break;
+            case "blank":
+                dotData.SetProperty("Color", JSONLevelLoader.ToJsonColor(DotColor.Blank));
+                break;
+            case "gem":
+                dotData.SetProperty("Color", (string)color);
+                break;
+            case "r-gem":
+                dotData.SetProperty("Color", (string)color);
+
+                dotData.SetProperty("DirectionX", (int)directionX);
+                dotData.SetProperty("DirectionY", (int)directionY);
+                break;
+        };
+        return dotData;
+    }
+    
+    
+}
---
+++ Assets/Scripts/DotsGameObjects/Factories/DotsFactory.cs
@@ -0,0 +1,64 @@
+using UnityEngine;
+using System;
+using Object = UnityEngine.Object;
+
+public class DotsFactory
+{
+    public static T CreateDotsGameObject<T>(DotsGameObjectData data)
+        where T : DotsGameObject
+    {
+        
+        DotsGameObject dotsGameObject = Object.Instantiate(JSONLevelLoader.FromJsonType(data.type));
+
+        if (dotsGameObject is IHittable hittable)
+        {
+            hittable.HitCount = data.hitCount;
+
+        }
+
+        if (dotsGameObject is IColorable colorable)
+        {
+            string color = data.GetProperty<string>("Color");
+            colorable.Color = JSONLevelLoader.FromJsonColor(color);
+        }
+
+        if (dotsGameObject is INumerable numberable)
+        {
+            int number = data.GetProperty<int>("Number");
+
+            numberable.InitialNumber = number;
+        }
+
+        if (dotsGameObject is IDirectional directional)
+        {
+            int directionX = data.GetProperty<int>("DirectionX");
+            int directionY = data.GetProperty<int>("DirectionY");
+
+            directional.DirectionX = directionX;
+            directional.DirectionY = directionY;
+
+        }
+
+        if(dotsGameObject is IMultiColorable multicolored)
+        {
+            string[] colors = data.GetProperty<string[]>("Colors");
+            multicolored.Colors = new DotColor[colors.Length];
+
+            for (int i = 0; i < colors.Length; i++)
+            {
+                multicolored.Colors[i] = JSONLevelLoader.FromJsonColor(colors[i]);
+            }
+        }
+
+        if(dotsGameObject is ISwitchable switchable)
+        {
+            bool isActive = data.GetProperty<bool>("IsActive");
+            switchable.IsActive = isActive;
+
+           
+        }
+
+        return (T)dotsGameObject;
+    }
+
+}
